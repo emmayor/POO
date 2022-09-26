d242b6063eceb82e98b46353fdc348cc7526b18d
Emmanuel Mayor <emayor@live.com.ar>
2022-09-26 02:13:52 -0300

    Added TP5

---
+++ tigres/TigreApp.java
@@ -0,0 +1,7 @@
+public class TigreApp {
+    public static void main(String[] args){
+        Tigre tigreVacio = new Tigre();
+        Tigre tigrePalermo = new Tigre("Delicado", "Fatal", "Infinita","Sanguinario","Hermoso");
+        tigrePalermo.imprimirDatos();
+    }
+}
