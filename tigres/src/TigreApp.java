278cd2f01b06f102b59c506307b13766064e8d7b
Emmanuel Mayor <emayor@live.com.ar>
2022-09-27 12:56:07 -0300

    Updated alquiler documentation

--- tigres/src/TigreApp.java
+++ tigres/src/TigreApp.java
@@ -1,6 +1,6 @@
 public class TigreApp {
     public static void main(String[] args){
-        Tigre tigrePalermo = new Tigre("Palermo", "Delicado", "Fatal", "Infinita","Sanguinario","Hermoso");
+        Tigre tigrePalermo = new Tigre("Palermo","Delicado","Fatal","Infinita","Sanguinario","Hermoso");
         Tigre miTigre = new Tigre("Villa del Parque","Atolondrado","Inofensivo","Poca","Tranquilo","Feo");
         tigrePalermo.imprimirDatos();
         miTigre.imprimirDatos();
