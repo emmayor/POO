ec9fd1c395cf8923ddf8cc54812e707df53a7607
Emmanuel Mayor <emayor@live.com.ar>
2022-10-04 01:59:35 -0300

    Configured VSCode to work with .class dir

--- ajedrez/src/Ajedrez.java
+++ ajedrez/src/Ajedrez.java
@@ -1,5 +1,4 @@
 public class Ajedrez {
-    
     private static Pieza[] instanciarPiezas(String color, String material) {
         // Instancia todas las 16 piezas de un mismo color y material
         int i = 0;
@@ -13,52 +12,54 @@ public class Ajedrez {
         int colReina = 3;
 
         if (color == "Negro") {
-            filaOtros = 7; 
-            filaPeon = 6; 
+            filaOtros = 7;
+            filaPeon = 6;
             colRey = 3;
             colReina = 4;
         }
 
-        for (i = 0; i < 8 ;i++){
-            piezas[i]= new Peon("Ladino", "Agresor",color, material, Tablero.calcularPosicion(filaPeon,i));
+        for (i = 0; i < 8; i++) {
+            piezas[i] = new Peon("Ladino", "Agresor", color, material, Tablero.calcularPosicion(filaPeon, i));
         }
 
         indice = i;
 
-        piezas[indice] = new Torre("Directa", "Homérica", color, material, Tablero.calcularPosicion(filaOtros,0));
+        piezas[indice] = new Torre("Directa", "Homérica", color, material, Tablero.calcularPosicion(filaOtros, 0));
         indice++;
-        piezas[indice] = new Torre("Directa", "Homérica", color, material, Tablero.calcularPosicion(filaOtros,7));
-        indice++;
-
-        piezas[indice] = new Caballo("Ligero", "???", color, material, Tablero.calcularPosicion(filaOtros,1));
-        indice++;
-        piezas[indice] = new Caballo("Ligero", "???", color, material, Tablero.calcularPosicion(filaOtros,6));
+        piezas[indice] = new Torre("Directa", "Homérica", color, material, Tablero.calcularPosicion(filaOtros, 7));
         indice++;
 
-        piezas[indice] = new Alfil("Oblicuo", "Sesgo", color, material, Tablero.calcularPosicion(filaOtros,2));
+        piezas[indice] = new Caballo("Ligero", "???", color, material, Tablero.calcularPosicion(filaOtros, 1));
         indice++;
-        piezas[indice] = new Alfil("Oblicuo", "Sesgo", color, material, Tablero.calcularPosicion(filaOtros,5));
+        piezas[indice] = new Caballo("Ligero", "???", color, material, Tablero.calcularPosicion(filaOtros, 6));
         indice++;
 
-        piezas[indice] = new Rey("Tenue", "Postrero", color, material, Tablero.calcularPosicion(filaOtros,colRey));
+        piezas[indice] = new Alfil("Oblicuo", "Sesgo", color, material, Tablero.calcularPosicion(filaOtros, 2));
         indice++;
-        piezas[indice] = new Reina("Armada", "Encarnizada", color, material, Tablero.calcularPosicion(filaOtros,colReina));
+        piezas[indice] = new Alfil("Oblicuo", "Sesgo", color, material, Tablero.calcularPosicion(filaOtros, 5));
+        indice++;
+
+        piezas[indice] = new Rey("Tenue", "Postrero", color, material, Tablero.calcularPosicion(filaOtros, colRey));
+        indice++;
+        piezas[indice] = new Reina("Armada", "Encarnizada", color, material, Tablero.calcularPosicion(filaOtros, colReina));
 
         return piezas;
     }
-    private static void imprimirCasilleros(Tablero tablero){
-        // Imprime el sistema de coordenadas y la disposición de colores de un tablero de 8 x 8
+
+    private static void imprimirCasilleros(Tablero tablero) {
+        // Imprime el sistema de coordenadas y la disposición de colores de un tablero
+        // de 8 x 8
         System.out.println("Tabla de posiciones:\n");
-        for (int i = 7; i >= 0; i--){
-            for (int j = 0; j < 8; j++){
-                System.out.print("|"+tablero.getCasilleros()[i][j].getPosicion());
+        for (int i = 7; i >= 0; i--) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print("|" + tablero.getCasilleros()[i][j].getPosicion());
             }
             System.out.println("\n------------------------");
         }
-        
+
         System.out.println("Disposición de los colores:\n");
-        for (int i = 7; i>=0; i--){
-            for (int j = 0; j<8; j++){
+        for (int i = 7; i >= 0; i--) {
+            for (int j = 0; j < 8; j++) {
                 if (tablero.getCasilleros()[i][j].getColor() == "Blanco") {
                     System.out.print("██");
                 } else {
@@ -69,30 +70,34 @@ public class Ajedrez {
         }
         System.out.println();
     }
-    private static void imprimirPieza(Pieza pieza){
+
+    private static void imprimirPieza(Pieza pieza) {
         // Imprime los atributos de una pieza
         String tipo = pieza.getClass().getSimpleName();
         String color = pieza.getColor();
         String material = pieza.getMaterial();
         String desplazamiento = pieza.getCapDesplazamiento();
-        String conducta= pieza.getConducta();
-        String posicion= pieza.getPosicion();
-        System.out.format("%-12s%-12s%-12s%-16s%-12s%-12s\n",tipo,color,material,desplazamiento,conducta,posicion);
+        String conducta = pieza.getConducta();
+        String posicion = pieza.getPosicion();
+        System.out.format("%-12s%-12s%-12s%-16s%-12s%-12s\n", tipo, color, material, desplazamiento, conducta,
+                posicion);
     }
-    private static void imprimirLado(Pieza[] piezas){
+
+    private static void imprimirLado(Pieza[] piezas) {
         // Imprime 16 piezas correspondientes a un lado del tablero
-        System.out.format("%-12s%-12s%-12s%-16s%-12s%-12s\n","TIPO","COLOR","MATERIAL","DESPLAZAMIENTO","CONDUCTA","POSICIÓN");
-        for(int i = 0; i < 16; i++){
+        System.out.format("%-12s%-12s%-12s%-16s%-12s%-12s\n", "TIPO", "COLOR", "MATERIAL", "DESPLAZAMIENTO", "CONDUCTA",
+                "POSICIÓN");
+        for (int i = 0; i < 16; i++) {
             imprimirPieza(piezas[i]);
         }
 
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Tablero tablero = new Tablero("Madera");
         System.out.println("Se ha creado un tablero de 8 x 8, cuyos casilleros se presentan de la siguiente manera:");
         imprimirCasilleros(tablero);
-        
+
         Pieza[] piezasBlancas = instanciarPiezas("Blanco", "Madera");
         System.out.println("\nSe han instanciado las piezas blancas:");
         imprimirLado(piezasBlancas);
@@ -105,6 +110,6 @@ public class Ajedrez {
         piezasNegras[4].Mover();
         piezasBlancas[12].Mover();
         piezasNegras[15].Mover();
-        
+
     }
-}   
+}
