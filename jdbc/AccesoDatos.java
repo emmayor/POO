2d9b7303a94c683a9dfe7756864ab94794af9dfe
Emmanuel Mayor <emayor@live.com.ar>
2022-10-23 17:20:13 -0300

    Added TP8

---
+++ jdbc/AccesoDatos.java
@@ -0,0 +1,91 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class AccesoDatos {
+
+	private String 		host;
+	private String  	usuario;
+	private String  	clave;
+	private int 		puerto;   
+	private String 	   	bd;
+	private String     	servidor; 
+	private Connection 	conexion;
+
+	private String getHost() {
+		return host;
+	}
+
+	private void setHost(String host) {
+		this.host = host;
+	}
+
+	private String getUsuario() {
+		return usuario;
+	}
+
+	private void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	private String getClave() {
+		return clave;
+	}
+
+	private void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	private int getPuerto() {
+		return puerto;
+	}
+
+	private void setPuerto(int puerto) {
+		this.puerto = puerto;
+	}
+
+	private String getBd() {
+		return bd;
+	}
+
+	private void setBd(String bd) {
+		this.bd = bd;
+	}
+
+	private String getServidor() {
+		return servidor;
+	}
+
+	private void setServidor(String servidor) {
+		this.servidor = servidor;
+	}
+
+	public Connection getConexion() {
+		return conexion;
+	}
+
+	public void setConexion(Connection conexion) {
+		this.conexion = conexion;
+	}
+
+	private void conectarBd(){
+		this.setServidor("jdbc:mariadb://"+this.getHost()+":"+this.getPuerto()+"/"+this.getBd());
+		try {
+	        conexion = DriverManager.getConnection(this.getServidor()+"?user="+this.getUsuario()+"&password="+this.getClave());
+	    } catch (SQLException error) {
+			System.err.println("ERROR: No se pudo realizar la conexión.");
+			error.printStackTrace();
+			System.exit(0);
+	    }
+	    System.out.println("Conectado a "+this.getBd());
+	}
+
+	public AccesoDatos(String host, String usuario, String clave, int puerto, String bd){
+		this.setHost(host);
+		this.setUsuario(usuario);
+		this.setClave(clave);
+		this.setPuerto(puerto);
+		this.setBd(bd);
+		conectarBd();
+	}    
+}
---
+++ jdbc/App.java
@@ -0,0 +1,62 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+// Comando: java -cp .:./bin/mariadb-java-client-3.0.8.jar App.java 
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        mostrarPiezasBD();
+    }
+
+    public static void mostrarPiezasBD()
+	{
+		//Objeto para ejecutar la consulta en la base de datos
+    	AccesoDatos accesoBD = null;
+    	Connection con = null;
+    	Statement sentencia = null;
+		ResultSet rs = null;
+		
+		// Se crea la consulta usando lenguaje SQL Estandar y almacenando en la variable query
+		String query = "SELECT * FROM tipopieza";
+        //En este bloque se ejecutan las sentencias necesarias para acceder a la BD y obtener la informacion
+        try {
+        	// Instancio un objeto de acceso a datos
+        	accesoBD = new AccesoDatos("localhost","admin","admin",3306,"ajedrez");
+			
+            //Obtener la conexion para poder generar la sentencia de consulta
+            con = accesoBD.getConexion();
+            sentencia = con.createStatement();
+            
+            //Ejecuta la consulta y almacena el resultado en rs
+            rs  = sentencia.executeQuery(query);
+           
+            // Procesa el resultSet y muestra la informacion obtenida desde la BD
+            while(rs.next()){
+            	
+            		int idPieza = rs.getInt("idTipoPieza");
+            		System.out.println("Pieza Nro: " + String.valueOf(idPieza)
+            		+ " tipo " + rs.getString("Descripciondepieza")	);            	         		
+            	            			
+			}    
+        } catch (SQLException error) {
+            System.err.println("ERROR: No se pudieron cargar los datos.");
+        }
+       	finally {
+        	try	{
+        		// Cierra el ResultSet
+        		if (rs!= null) rs.close();
+        		// Cierra la sentencia
+        		if (sentencia!= null) sentencia.close();
+        		// Cierra la conexion
+        		if (con != null) con.close();
+        		
+        	}catch (SQLException error)
+        	{
+        		System.err.println("ERROR: No se pudo cerrar la conexión.");
+        	}
+        }
+	}
+	
+}
